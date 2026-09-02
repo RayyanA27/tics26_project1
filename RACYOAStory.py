bc3e34a4942123aa5d3b031535a987a9f68585b9
RayyanA27 <rabbasi074@phm.k12.in.us>
2026-09-02 13:46:08 -0400

    I added in my while loop, bool, and f strings too enhance my code.

--- RACYOAStory.py
+++ RACYOAStory.py
@@ -1,55 +1,85 @@
-devicelist = ["PC", "Phone", "Ipad", "Tablet",]
+devicelist = ["PC", "Phone", "Ipad", "Tablet"]
+rating = [1/3, 2/3, 3/3]
 health = int(100)
 
-device = input("What device are you playing on?", devicelist)
-name = input("Welcome", device, "user", "What is your name?")
+device = input(f"What device are you playing on?{devicelist}")
+name = input(f"Welcome {device} user: What is your name?")
 
-print(name, "You are being chased by zombies. The zombies are closing in on you, but you have three options. You can choose to run, fight, or hide")
+print([name], f"You are being chased by zombies. The zombies are closing in on you, but you have three options. You can choose to run, fight, or hide")
 
 decision = input("What will you do. run, fight, or hide?")
 
-if decision == "run":
-    print("You widened the distance between you and the zombies.")
-    print("Health:", health)
+alive = True
 
-    decision2 = input("What will you do. run, fight, or hide")
+while alive == True:
 
-    if decision2 == "run":
-        print("You exerted too much energy. Eventually the zombies caught up, and you perished. ")
+
+    if decision == "run":
+        print("You widened the distance between you and the zombies.")
+        print(f"Health:", {health})
+
+
+        decision2 = input("What will you do. run, fight, or hide?")
+
+
+        if decision2 == "run":
+            print("You exerted too much energy. Eventually the zombies caught up, and you perished. ")
+            health2 = health - 100
+            print(f"Health:", {health2})
+            ending = print("Better luck next time!")
+            alive = False
+
+        if decision2 == "fight":
+            print("You could not handle the sheer amount of zombies. You gave a valiant effort.")
+            health2 = health - 100
+            print(f"Health:", {health2})
+            ending = print("At least you made it this far!")
+            alive = False
+            
+        if decision2 == "hide":
+            print("You were not detected by the zombies. Good work. You survived!")
+            print(f"Health:", {health2})
+
+
+            
+
+
+    elif decision == "fight": 
+        print("There were too many zombies to fight off. You died an honorable death. R.I.P")
         health2 = health - 100
-        print("Health:", health2)
-        ending = print("Better luck next time!")
+        print(f"Health:", {health2})
+        alive = False
 
-    if decision2 == "fight":
-        print("You could not handle the sheer amount of zombies. You gave a valiant effort.")
+
+    elif decision == "hide": 
+        print("You successfully hid from the zombies. Good work. You survived!")
+        print(f"Health:", {health})
+
+
+        decision2 = input("What will you do. run, fight, or hide?")
+
+
+        if decision2 == "run":
+                print("You exerted too much energy. You were a warrior though!")
+                health2 = health - 100
+                print(f"Health:", {health2})
+                alive = False
+
+        if decision2 == "fight":
+                print("You fought off all of the zombies. Good work. You survived!")
+                health2 = print(f"Health:", {health})
+
+        if decision2 == "hide":
+                print("You were caught by the zombies. You can't hide forever!")
+                health2 = health - 100
+                print(f"Health:", {health2})
+                alive = False
+
+
+    else:
+        print("Invalid decision. You died because you waited for the zombies to catch up")
         health2 = health - 100
-        print("Health:", health2)
-        ending = print("At least you made it this far!")
-        
-    if decision2 == "hide":
-        print("You were not detected by the zombies. Good work.")
-        print("Health:", health)
-
-
-        
-
-
-elif decision == "fight": 
-    print("There were too many zombies to fight off. You died an honorable death. R.I.P")
-    health2 = health - 100
-    print("Health:", health2)
-
-
-elif decision == "hide": 
-    print("You successfully hid from the zombies")
-    print("Health:", health)
-
-
-else:
-    print("Invalid decision. You died because you waited for the zombies to catch up")
-    health2 = health - 100
-    print(health2)
-    ending = print("Game Over!")
-
-
-
+        print(health2)
+        ending = print("Game Over!")
+        print(f"Out of respect for Jayden Daniels, our rating will not be out of 5. Instead it will be out of 3. Rate the game.", {rating}, "Thank you", {name}, "for playing.")
+print(ending)
